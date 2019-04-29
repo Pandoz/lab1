f7d2f1d27edf7c9990ff82e7d03b5e76983ca6cb
Kr4s4vcheGG <47522796+Pandoz@users.noreply.github.com>
2019-04-29 13:56:46 +0300

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -1,35 +1,43 @@
-#include <cassert> // for assert()
 #include <iostream>
 #include "Complex.h"
 
+bool check(double x, double y)//Проверка
+{
+	if (x > 1000 || x < -1000 || y > 1000 || y < -1000)
+	{
+		cout << "Вы ввели неверное число, попробуйте еще раз\n";
+		return 1;
+	}
+	return 0;
+}
+
 Complex addComplex(const Complex &x, const Complex &y)
 {
-	Complex answer;
-	answer.re = x.re + y.re;
-	answer.im = x.im + y.im;
-	return answer;
+	Complex qwert;
+	qwert.re = x.re + y.re;
+	qwert.im = x.im + y.im;
+	return qwert;
 }
 
 Complex subtractComplex(const Complex &x, const Complex &y)
 {
-	Complex answer;
-	answer.re = x.re - y.re;
-	answer.im = x.im - y.im;
-	return answer;
+	Complex qwert;
+	qwert.re = x.re - y.re;
+	qwert.im = x.im - y.im;
+	return qwert;
 }
 
 Complex multiplyComplex(const Complex &x, const Complex &y)
 {
-	Complex answer;
-	answer.re = x.re * y.re - x.im * y.im;
-	answer.im = x.re * y.im + x.im * y.re;
-	return answer;
+	Complex qwert;
+	qwert.re = x.re * y.re - x.im * y.im;
+	qwert.im = x.re * y.im + x.im * y.re;
+	return qwert;
 }
 
 Complex divideComplex(const Complex &x, const Complex &y)
 {
-	assert((y.re != 0) && (y.im != 0));
-	Complex answer;
+	Complex qwert;
 	answer.re = (x.re * y.re + x.im * y.im) / (y.re * y.re + y.im * y.im);
-	answer.im = (x.im * y.re - x.re * y.im) / (y.re * y.re + y.im * y.im);
-	return answer;
+	qwert.im = (x.im * y.re - x.re * y.im) / (y.re * y.re + y.im * y.im);
+	return qwert;
