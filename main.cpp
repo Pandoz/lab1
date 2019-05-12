2d4c229a043b2949433b4b7d909f4c24384b4978
Kr4s4vcheGG <47522796+Pandoz@users.noreply.github.com>
2019-05-13 02:29:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,69 +4,66 @@
 using namespace std;
 int main()
 {
-	system("chcp 1251");
-	Complex N1{ };
-	Complex N2{ };
+	chislo N1{ }; 
+	chislo N2{ };
 	char oper{ };
 	
 	cout << "Введите первое комплексное число: ";
-	cin >> x.re >> x.im;
+	cin >> x.re >> x.im; //вводим оба значения первого числа
 	cout << "Введите второе комплексное число: ";
-	cin >> y.re >> y.im;
+	cin >> y.re >> y.im; //вводим оба значения второго числа
 	}
-	Complex N1 = { x.re, x.im }
-	Complex N2 = { y.re, y.im };
+	chislo N1 = { x.re, x.im }; //первое комплексное число
+	chislo N2 = { y.re, y.im }; //второе
 	
 	cout << "Введите символ операции над числами: ";
-	cin >> oper;
+	cin >> oper; //выбираем операци.
 
 	}
 	switch (oper)
 	{
 	case '+':
-		pluss(N1, N2);
-		break;
+		plus(N1, N2);
+		break; //выполняем функцию сложения
 	case '-':
-		substruct(N1, N2);
-		break;
+		minus(N1, N2);
+		break; //выполняем функцию вычитания
 	case '*':
-		multiplication(N1, N2);
-		break;
+		ymnoz(N1, N2);
+		break; //выполняем функцию умножения
 	case '/':
-		devide(N1, N2);
-		break;
+		delen(N1, N2);
+		break; //выполняем функцию деления
 
 	default:
-		cout << "Данной операции не существует\n Вычисления не возможны";
+		cout << "Данной операции не существует\n Вычисления не возможны"; //иначе вывод
 	}
-	cout << "Чтение комплексных чисел из файла complex.txt...\n";
+	cout << "Чтение комплексных чисел из файла complex.txt...\n"; //часть 2 задание
 	ifstream file;
-	file.open("complex.txt");
+	file.open("complex.txt"); //открываем файл
 	{
 		int f, max = -1;
 		int maxIndex;
 		file >> f; //считываемм значения с файла комлекс.тхт
 		int n;
 
-		Complex *p = new Complex[n]; //заполняем массив
-		for (int i = 0; i < n; i++)
-		{
+		Complex *p = new Complex[n]; 
+		for (int i = 0; i < n; i++) //считываем количество комплексных чисел в переменную n //первое число в файле - кол-во  //а дальше числа которые идут в сам массив
+		{ //а сами числа в массив
 			file >> f;
-			p[i].re = f; //два числа из файла         
-			//первое число в файле количество комплекс чисел
-			file >> f;                        //а дальше числа которые идут в сам массив
-			p[i].im = f; // два числа из файла
-			cout << i + 1 << " число: " << p[i].re << " + (" << p[i].im << ")i\n"; //результат их
+			p[i].re = f; //первая часть числа      
+			file >> f;                    
+			p[i].im = f; //вторая часть числа
+			cout << i + 1 << " число: " << p[i].re << " + (" << p[i].im << ")i\n"; //само число
 
 			if (sqrt(p[i].re * p[i].re + p[i].im * p[i].im) > max)
 			{
-				max = sqrt(p[i].re * p[i].re + p[i].im * p[i].im);
+				max = sqrt(p[i].re * p[i].re + p[i].im * p[i].im); //ищем комплексное число с макс. модулем в массиве p
 				maxIndex = i;
 			}
 		}
 		cout << "Комплексное число с максимальным модулем: " << p[maxIndex].re << " + (" << p[maxIndex].im << ")i\n";
-		cout << "Модуль равен: " << max << '\n';
-	}
+	} //выводим максимальное число с модулем
 	file.close();
 	return 0; 
 
