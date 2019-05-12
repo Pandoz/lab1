4746f82a7c8f26bdb7f16279bed43a3522f4d00d
Kr4s4vcheGG <47522796+Pandoz@users.noreply.github.com>
2019-05-13 02:18:30 +0300

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -2,33 +2,33 @@
 #include "Complex.h"
 
 //файл с реализациями функций
-void pluss(Complex N1, Complex N2)//Операция сложения между двумя структурами
+void plus(chislo N1, chislo N2)//Операция сложения между двумя структурами
 {
-	Complex a; //cоздаем новую струкртуру, в которой производим все вычисления и выводим результат
+	chislo a; //cоздаем новую струкртуру, в которой производим все вычисления и выводим результат
 	a.re = x.re + y.re; //первую часть числа
 	a.im = x.im + y.im; //вторую часть числа
 	cout << a.re << " + (" << a.im << ")i\n"; //вывод результата
 }
 
-void substruct(Complex N1, Complex N2)//Операция вычетания
+void minus(chislo N1, chislo N2)//Операция вычетания
 {
-	Complex a;
+	chislo a;
 	a.re = x.re - y.re;
 	a.im = x.im - y.im;
 	cout << a.re << " + (" << a.im << ")i\n";
 }
 
-void multiplication(Complex N1, Complex N2)//Операция умножения
+void ymnoz(chislo N1, chislo N2)//Операция умножения
 {
-	Complex a;
+	chislo a;
 	a.re = x.re * y.re - x.im * y.im;
 	a.im = x.re * y.im + x.im * y.re;
 	cout << a.re << " + (" << a.im << ")i\n";
 }
 
-void devide(Complex N1, Complex N2)//Операция деления
+void delen(chislo N1, chislo N2)//Операция деления
 {
-	Complex a;
+	chislo a;
 	a.re = (x.re * y.re + x.im * y.im) / (y.re * y.re + y.im * y.im);
 	a.im = (x.im * y.re - x.re * y.im) / (y.re * y.re + y.im * y.im);
 	cout << a.re << " + (" << a.im << ")i\n";
