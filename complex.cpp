f67bd2f14fea0051bc9c7b48612154501aad6ed7
Kr4s4vcheGG <47522796+Pandoz@users.noreply.github.com>
2019-05-13 02:14:03 +0300

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 #include "Complex.h"
 
-
-void pluss(Complex N1, Complex N2)//Операция сложения
+//файл с реализациями функций
+void pluss(Complex N1, Complex N2)//Операция сложения между двумя структурами
 {
-	Complex qwert;
-	qwert.re = x.re + y.re;
-	qwert.im = x.im + y.im;
-	cout << qwert.re << " + (" << qwert.im << ")i\n";
+	Complex a; //cоздаем новую струкртуру, в которой производим все вычисления и выводим результат
+	a.re = x.re + y.re; //первую часть числа
+	a.im = x.im + y.im; //вторую часть числа
+	cout << a.re << " + (" << a.im << ")i\n"; //вывод результата
 }
 
 void substruct(Complex N1, Complex N2)//Операция вычетания
 {
-	Complex qwert;
-	qwert.re = x.re - y.re;
-	qwert.im = x.im - y.im;
-	cout << qwert.re << " + (" << qwert.im << ")i\n";
+	Complex a;
+	a.re = x.re - y.re;
+	a.im = x.im - y.im;
+	cout << a.re << " + (" << a.im << ")i\n";
 }
 
 void multiplication(Complex N1, Complex N2)//Операция умножения
 {
 	Complex qwert;
-	qwert.re = x.re * y.re - x.im * y.im;
-	qwert.im = x.re * y.im + x.im * y.re;
-	cout << qwert.re << " + (" << qwert.im << ")i\n";
+	a.re = x.re * y.re - x.im * y.im;
+	a.im = x.re * y.im + x.im * y.re;
+	cout << a.re << " + (" << a.im << ")i\n";
 }
 
 void devide(Complex N1, Complex N2)//Операция деления
 {
-	Complex qwert;
-	qwert.re = (x.re * y.re + x.im * y.im) / (y.re * y.re + y.im * y.im);
-	qwert.im = (x.im * y.re - x.re * y.im) / (y.re * y.re + y.im * y.im);
-	cout << qwert.re << " + (" << qwert.im << ")i\n";
+	Complex a;
+	a.re = (x.re * y.re + x.im * y.im) / (y.re * y.re + y.im * y.im);
+	a.im = (x.im * y.re - x.re * y.im) / (y.re * y.re + y.im * y.im);
+	cout << a.re << " + (" << a.im << ")i\n";
 }
